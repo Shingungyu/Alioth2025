412c6d4fd595afb3662dc2bce9a463ea118486ba
hansb123a <epic7ebung@gmail.com>
2024-11-22 18:08:30 +0900

    스토리관련
    스토리씬 및 정지기능추가

---
+++ Assets/Script/Story_Script/Stoty11.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stoty11 : MonoBehaviour
+{
+    public Text ChatText;
+    public Text CharacterName;
+
+    public string writerText = "";
+
+    GameObject Canvas;
+
+    void Start()
+    {
+        Canvas = GameObject.Find("Canvas");
+        StartCoroutine(TextPractice());
+        Time.timeScale = 0f;
+    }
+
+
+    IEnumerator NormalChat(string narrator, string narration)
+    {
+        int a = 0;
+        CharacterName.text = narrator;
+        writerText = "";
+
+        for (a = 0; a < narration.Length; a++)
+        {
+            writerText += narration[a];
+            ChatText.text = writerText;
+            yield return null;
+        }
+
+        while (true)
+        {
+            if (Input.GetKeyDown("z"))
+            {
+                break;
+            }
+            yield return null;
+        }
+
+    }
+
+    IEnumerator TextPractice()
+    {
+        yield return StartCoroutine(NormalChat("Alioth", "여긴..."));
+        yield return StartCoroutine(NormalChat("Alioth", "두오의 행성인거 같아."));
+        yield return StartCoroutine(NormalChat("Alioth", "나아가야겠어. 두오의 힘을 찾으러"));
+        Time.timeScale = 1f;
+        Canvas.SetActive(false);  // 대사 출력 후 캔버스 비활성화
+    }
+}
