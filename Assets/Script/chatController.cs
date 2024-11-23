43121a05b33a36d37482c2b231ebe1797e2f8a5b
hansb123a <epic7ebung@gmail.com>
2024-11-23 21:20:01 +0900

    4스테이지 추가

--- Assets/Script/chatController.cs
+++ Assets/Script/chatController.cs
@@ -49,10 +49,10 @@ public class chatController : MonoBehaviour
     IEnumerator TextPractice() {
 
         yield return StartCoroutine(NormalChat("Alioth", "........."));
-        yield return StartCoroutine(NormalChat("Alioth", "여긴 어디지?"));
-        yield return StartCoroutine(NormalChat("Alioth", "좀 추워진거 같은데.."));
-        yield return StartCoroutine(NormalChat("Alioth", "아까보다 점점 더 힘이 나는것 같기도 한거같아."));
-        yield return StartCoroutine(NormalChat("Alioth", "앞으로 3명만 더 구하면 되는거야. 힘내자."));
+        yield return StartCoroutine(NormalChat("Alioth", "뭔가 이상해..."));
+        yield return StartCoroutine(NormalChat("Alioth", "메그레즈의 행성은 분명 이런 모양새가 아니었는데.."));
+        yield return StartCoroutine(NormalChat("Alioth", "아까보다 오히려 더 추워진 것 같아."));
+        yield return StartCoroutine(NormalChat("Alioth", "정신차리자. 앞으로 3명만 더 구하면 되는거야. 힘내자."));
         Time.timeScale = 1f;
         Canvas.SetActive(false);
     }
