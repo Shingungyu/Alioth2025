6e994051735fcf19bdbf8d0f591478433624690f
hansb123a <epic7ebung@gmail.com>
2025-09-10 17:23:14 +0900

    Update WallTrigger.cs

--- Assets/Script/WallTrigger.cs
+++ Assets/Script/WallTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WallTrigger : MonoBehaviour
 {
     public PlayerController controller;
-
+    //½Â¹ü commit test
     private void Awake()
     {
         controller = transform.GetComponentInParent<PlayerController>();
