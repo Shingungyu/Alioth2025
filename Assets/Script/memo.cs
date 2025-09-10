1639c350d58e912451d874b35515e6cfc3c3aa0b
Shingungyu <sgg991227@naver.com>
2025-09-10 17:14:12 +0900

    2025_0910_first_commit
    lessugo

---
+++ Assets/Script/memo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class memo : MonoBehaviour
+{
+    //깃허브 확인용 메모중_2025.09.10_0513
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
